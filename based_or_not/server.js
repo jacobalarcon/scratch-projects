383026ed5f158d43bf63bae4107af4b38fd612cf
Jacob Alarcon <jacobalarcon@gmail.com>
2023-03-31 20:07:53 -0500

    Add basic route to database

--- based_or_not/server.js
+++ based_or_not/server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('testing');
 });
 
+// get posts from the database
+app.get('/getPosts', (req, res) => {
+  res.send('database data that users will vote on');
+});
 app.listen(port, () => {
   console.log('Listening on port 3000');
 });
